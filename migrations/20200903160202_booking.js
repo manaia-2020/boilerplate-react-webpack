38260421c14dc109d3d2ad1ab33ccf3a5f3a3673
Your Namemax-remacle <remacle.maxime@gmail.com>
2020-09-05 11:39:45 +1200

    add migrations

--- migrations/20200903160202_booking.js
+++ migrations/20200903160202_booking.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('Bookings',(table) =>{
+      table.increments('id').primary()
+      table.string('name')
+      table.integer('number')
+      table.date('dateCreated')
+      table.datetime('start').unique()
+      table.datetime('end').unique()
+      table.string('comments')
+  })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable('Bookings')
 };

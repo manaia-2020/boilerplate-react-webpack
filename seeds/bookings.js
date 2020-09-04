f47f77047bf3bca075e0f2d6f6abb3d6e344a0ba
Your Namemax-remacle <remacle.maxime@gmail.com>
2020-09-05 11:52:32 +1200

    add seeds

---
+++ seeds/bookings.js
@@ -0,0 +1,19 @@
+exports.seed = function (knex) {
+  // Deletes ALL existing entries
+  return knex("Bookings")
+    .del()
+    .then(function () {
+      // Inserts seed entries
+      return knex("Bookings").insert([
+        {
+          id: 1,
+          name: "Maxime",
+          number: 278433871,
+          dateCreated: new Date(Date.now()),
+          start: new Date(2020, 8, 6, 15, 0),
+          end: new Date(2020, 8, 6, 15, 0),
+          comments: "I wanna go flying",
+        },
+      ]);
+    });
+};
